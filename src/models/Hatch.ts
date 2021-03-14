cf3052230885818909e5cb610efa81d553e668af
PJColombo <paulo.colombo@pm.me>
2021-03-14 22:17:17 +0100

    Hatch model object documented

--- src/models/Hatch.ts
+++ src/models/Hatch.ts
@@ -13,7 +13,7 @@ import { buildContributorId } from '../helpers'
 
 const HATCH_ORACLE_APP = 'hatch-oracle'
 
-export default class Hatch {
+class Hatch {
   #app: App
   #connector: IHatchConnector
 
@@ -22,14 +22,31 @@ export default class Hatch {
     this.#app = app
   }
 
+  /**
+   * Disconnect the connector from the subgraph.
+   */
   async disconnect(): Promise<void> {
     this.#connector.disconnect()
   }
 
+  /**
+   * Fetch the GeneralConfig object that contains configuration data both of
+   * the hatch and the hatch oracle.
+   * @returns {Promise<GeneralConfig>} A promise that resolves to a GeneralConfig
+   * entity object.
+   */
   generalConfig(): Promise<GeneralConfig> {
     return this.#connector.generalConfig(this.#app.organization.address)
   }
 
+  /**
+   * Subscribe to the GeneralConfig object updates that contains configuration data both
+   * of the hatch and the hatch oracle.
+   * @param {SubscriptionCallback<GeneralConfig>} callback A callback function that
+   * receives the updated general configuration data.
+   * @returns {SubscriptionHandler} A function handler use to cancel the
+   * subscription at any time.
+   */
   onGeneralConfig(
     callback: SubscriptionCallback<GeneralConfig>
   ): SubscriptionHandler {
@@ -39,6 +56,11 @@ export default class Hatch {
     )
   }
 
+  /**
+   * Fetch multiple contributors.
+   * @param {Object} param0 A filtering options object for the query.
+   * @returns {Promise<Contributor[]>} A promise which resolves to an array of Contributor objects.
+   */
   contributors({
     first = 1000,
     skip = 0,
@@ -54,6 +76,14 @@ export default class Hatch {
     )
   }
 
+  /**
+   * Subscribe to multiple contributors data updates.
+   * @param {Object} param0 A filtering options object for the query.
+   * @param {SubscriptionCallback<Contributor[]>} callback A callback function that
+   * receives the updated array of Contributor objects.
+   * @returns {SubscriptionHandler} A function handler used to cancel the
+   * subscription at any time.
+   */
   onContributors(
     { first = 1000, skip = 0, orderBy = 'value', orderDirection = 'desc' } = {},
     callback: SubscriptionCallback<Contributor[]>
@@ -68,12 +98,25 @@ export default class Hatch {
     )
   }
 
+  /**
+   * Fetch a single contributor.
+   * @param {string} contributor A string that contains the contributor address.
+   * @returns {Contributor} A promise that resolves to the searched contributor.
+   */
   contributor(contributor = ''): Promise<Contributor> {
     return this.#connector.contributor(
       buildContributorId(this.#app.address, contributor)
     )
   }
 
+  /**
+   * Subscribe to a single contributor data updates.
+   * @param {string} contributor The contributor address.
+   * @param {SubscriptionCallback<Contribution>} callback A callback function that
+   * receives the updated Contributor object data.
+   * @returns {SubscriptionHandler} A function handler used to cancel the subscription
+   * at any time.
+   */
   onContributor(
     contributor = '',
     callback: SubscriptionCallback<Contributor>
@@ -84,6 +127,11 @@ export default class Hatch {
     )
   }
 
+  /**
+   * Fetch multiple contributions.
+   * @param {Object} param0 A filtering options object for the query.
+   * @returns {Promise<Contribution[]>} A promise that resolves to an array of Contribution objects.
+   */
   contributions({
     contributor = '',
     first = 1000,
@@ -101,6 +149,14 @@ export default class Hatch {
     )
   }
 
+  /**
+   * Subscribe to multiple contributions data updates.
+   * @param {Object} param0 A filtering options object for the query.
+   * @param {SubscriptionCallback<Contribution[]} callback A callback function that
+   * receives the updated array of Contribution objects.
+   * @returns {SubscriptionHandler} A function handler used to cancel the subscription
+   * at any time.
+   */
   onContributions(
     {
       contributor = '',
@@ -122,14 +178,36 @@ export default class Hatch {
     )
   }
 
-  async open(signerAddress: string): Promise<ForwardingPath> {
+  /**
+   * Open the hatch.
+   * @param {Address} signerAddress A string that contains the transaction signer address.
+   * @returns {Promise<ForwardingPath>} A promise that resolves to a ForwardingPath
+   * object which contains all the transactions needed to be signed in order to open
+   * the hatch.
+   */
+  async open(signerAddress: Address): Promise<ForwardingPath> {
     return this.#app.intent('open', [], { actAs: signerAddress })
   }
 
-  async close(signerAddress: string): Promise<ForwardingPath> {
+  /**
+   * Close the hatch.
+   * @param {Address} signerAddress A string that contains the transaction signer address.
+   * @returns {Promise<ForwardingPath>} A promise that resolves to a ForwardingPath
+   * object which contains all the transactions needed to be signed in order to close
+   * the hatch.
+   */
+  async close(signerAddress: Address): Promise<ForwardingPath> {
     return this.#app.intent('close', [], { actAs: signerAddress })
   }
 
+  /**
+   * Contribute to the hatch.
+   * @param {Address} contributor The address of the
+   * contributing entity.
+   * @param {string} value A string that contains the amount of tokens to contribute.
+   * @returns {Promise<ForwardingPath>} A promise that resolves to a ForwardingPath object
+   * which contains all the transactions needed to be signed in order to contribute to the hatch.
+   */
   async contribute(
     contributor: Address,
     value: string
@@ -156,6 +234,14 @@ export default class Hatch {
     return intent
   }
 
+  /**
+   * Refund a contributor hatch contribution.
+   * @param {Address} contributor The address of the contributor.
+   * @param {number} vestedPurchaseId The id of the vested hatch tokens purchase.
+   * @returns {Promise<ForwardingPath>} A promise that resolves to a Forwarding Path
+   * object which contains all the transactions needed to be signed in order to get
+   * a refund from the hatch.
+   */
   async refund(
     contributor: Address,
     vestedPurchaseId: number
@@ -165,12 +251,25 @@ export default class Hatch {
     })
   }
 
+  /**
+   * Fetch a holder's contribution token balance.
+   * @param {Address} entity A string that contains the token holder address.
+   * @returns {Promise<BigNumber>} A promise that resolves to the holder
+   * token balance.
+   */
   async tokenBalance(entity: Address): Promise<BigNumber> {
     const hatch = this.#app.contract()
 
     return hatch.balanceOf(entity)
   }
 
+  /**
+   * Return the allowed amount of contribution tokens someone can contribute to the
+   * hatch based on how much score tokens he has.
+   * @param {Address} contributor A string which contains the address of the contributor.
+   * @returns {Promise<BigNumber>} A promise that resolves to the allowed
+   * contribution token amount.
+   */
   async getAllowedContributionAmount(contributor: Address): Promise<BigNumber> {
     const hatchOracleApp = await this.#app.organization.app(HATCH_ORACLE_APP)
     const hatchOracle = hatchOracleApp.contract()
@@ -178,3 +277,5 @@ export default class Hatch {
     return hatchOracle.allowance(contributor)
   }
 }
+
+export default Hatch

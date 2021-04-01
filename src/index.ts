928971f03822018b7eb0640b182b678ee5b7a413
PJColombo <paulo.colombo@pm.me>
2021-04-01 03:54:04 +0200

    Token entity renamed for better clarity

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as HatchConfig } from './models/HatchConfig'
 export { default as Contribution } from './models/Contribution'
-export { default as Token } from './models/Token'
+export { default as ERC20Token } from './models/ERC20Token'
 export { default as Hatch } from './models/Hatch'
 export { default as HatchConnectorTheGraph } from './thegraph/connector'
 export { default } from './connect'
--- src/models/HatchOracleConfig.ts
+++ src/models/HatchOracleConfig.ts
@@ -1,14 +1,14 @@
 import { HatchOracleConfigData } from '../types'
-import Token from './Token'
+import ERC20Token from './ERC20Token'
 
 class HatchOracleConfig {
   readonly id: string
-  readonly scoreToken: Token
+  readonly scoreToken: ERC20Token
   readonly ratio: number
 
   constructor(data: HatchOracleConfigData) {
     this.id = data.id
-    this.scoreToken = data.scoreToken
+    this.scoreToken = new ERC20Token(data.scoreToken)
     this.ratio = data.ratio
   }
 }

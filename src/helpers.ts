14ff556c3aa408b16414954698aa3150d60ed2d3
PJColombo <paulo.colombo@pm.me>
2021-04-01 04:02:10 +0200

    Connector: Fetching token data operations implemented (impact hours rewards, token balances and maximum allowed contribution amount)

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,7 @@
+export const IMPACT_HOURS_APP = 'impact-hours-beta'
+export const HATCH_APP = 'marketplace-hatch'
+export const HATCH_ORACLE_APP = 'hatch-oracle'
+
 export function buildContributorId(
   appAddress: string,
   contributor: string
